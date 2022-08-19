3cbea2d6b52698c1c34158b8eeb59f19903c161e
Olha <zemlakova@gmail.com>
2022-08-19 23:21:55 +0200

    WIP homework

---
+++ 37/assets/scripts/main.js
@@ -0,0 +1,22 @@
+$(function(){
+    $(".slider-top").slick({
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      dotsClass: 'slick-dots top-dots'
+    });
+  });
+
+  $(function(){
+    $('.slider-mid').slick({
+        dots: false,
+        slidesToShow: 6,
+        slidesToScroll: 6,
+        adaptiveHeight: true,
+        arrows: true,
+        accessibility: true
+    })
+})
