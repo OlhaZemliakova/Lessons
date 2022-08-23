de9d55e0f5f1fa19c057a7991fe6afb68737cc07
Olha <zemlakova@gmail.com>
2022-08-23 23:06:14 +0200

    WIP homework

--- 37/assets/scripts/main.js
+++ 37/assets/scripts/main.js
@@ -1,22 +1,44 @@
-$(function(){
-    $(".baner__carousel").slick({
-      dots: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      arrows: false,
-      dotsClass: 'slick-dots top-dots'
-    });
+function banerSlider() {
+  $(".baner__carousel").slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dotsClass: 'slick-dots top-dots'
   });
+};
 
-  $(function(){
-    $('.slider-mid').slick({
-        dots: false,
-        slidesToShow: 6,
-        slidesToScroll: 6,
-        adaptiveHeight: true,
-        arrows: true,
-        accessibility: true
-    })
-})
+function productsSlider() {
+  $('.products__carousel').slick({
+    dots: false,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    adaptiveHeight: true,
+    arrows: true,
+    accessibility: true,
+    prevArrow: '.products__slick-prev',
+    nextArrow: '.products__slick-prev',
+  })
+}
+
+function partnersSlider() {
+  $('.partners__carousel').slick({
+    dots: false,
+    slidesToShow: 9,
+    slidesToScroll: 1,
+    adaptiveHeight: true,
+    arrows: true,
+    accessibility: true,
+    prevArrow: '.partners__slick-prev',
+    nextArrow: '.partners__slick-next',
+  })
+}
+
+
+$(document).ready(function () {
+  banerSlider();
+  productsSlider();
+  partnersSlider();
+});
