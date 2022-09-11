79a574be50eb00407635187ab24aaa638638ee92
Olha <zemlakova@gmail.com>
2022-09-11 13:30:55 +0200

    created a project on vue

---
+++ exam-3/wallheven/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import Search from './components/Search.vue';
+import SearchResult from './components/SearchResult.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Search,
+    SearchResult,
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
